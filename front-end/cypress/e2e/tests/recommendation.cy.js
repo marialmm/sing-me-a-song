d789bb543b0b8a87c5172bf2448ad16980b624e4
Maria Luiza <maria.lmm.975@gmail.com>
2022-08-01 17:28:31 -0300

    feat: add Downvote recommendation cypress test

--- front-end/cypress/e2e/tests/recommendation.cy.js
+++ front-end/cypress/e2e/tests/recommendation.cy.js
@@ -45,3 +45,37 @@ describe("Upvote recommendation", () => {
         cy.get("article .score").should("contain.text", "1");
     });
 });
+
+describe("Downvote recommendation", () => {
+    it("Should downvote a recommendation", () => {
+        cy.createRecommendation();
+        cy.intercept("GET", "/recommendations").as("getRecommendations");
+        cy.intercept("POST", "/recommendations/1/downvote").as(
+            "downvoteRecommendation"
+        );
+
+        cy.visit("http://localhost:3000");
+        cy.wait("@getRecommendations");
+        cy.get("article .downvote").click();
+        cy.wait("@downvoteRecommendation");
+        cy.get("article .score").should("contain.text", -1);
+    });
+
+    it("Should remove a recommendation", () => {
+        cy.createRecommendation();
+        cy.intercept("GET", "/recommendations").as("getRecommendations");
+        cy.intercept("POST", "/recommendations/1/downvote").as(
+            "downvoteRecommendation"
+        );
+
+        cy.visit("http://localhost:3000");
+        cy.wait("@getRecommendations");
+
+        for (let i = 0; i < 6; i++) {
+            cy.get("article .downvote").click();
+            cy.wait("@downvoteRecommendation");
+        }
+
+        cy.get("article").should("not.exist");
+    });
+});
