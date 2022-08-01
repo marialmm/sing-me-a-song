848e407f113133af29ea4d2acfc1e5293a9a3dcb
Maria Luiza <maria.lmm.975@gmail.com>
2022-08-01 10:54:18 -0300

    feat: add Downvote recommendation tests

--- back-end/tests/unit/recommendationsService.test.js
+++ back-end/tests/unit/recommendationsService.test.js
@@ -6,6 +6,10 @@ import * as recommendationsFactory from "../factories/recommendationsFactory.js"
 
 jest.mock("../../src/repositories/recommendationRepository");
 
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
 describe("Create recommendation tests", () => {
     it("Should create a recommendation", async () => {
         jest.spyOn(
@@ -39,6 +43,7 @@ describe("Create recommendation tests", () => {
             type: "conflict",
             message: "Recommendations names must be unique",
         });
+        expect(recommendationRepository.create).not.toHaveBeenCalled();
     });
 });
 
@@ -47,10 +52,9 @@ describe("Upvote recommendation tests", () => {
         jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce({
             id: 1,
         });
-        jest.spyOn(
-            recommendationRepository,
-            "updateScore"
-        ).mockImplementation(() => {});
+        jest.spyOn(recommendationRepository, "updateScore").mockImplementation(
+            () => {}
+        );
 
         await recommendationService.upvote(1);
 
@@ -58,10 +62,59 @@ describe("Upvote recommendation tests", () => {
     });
 
     it("Given a id that doesn't exist, should throw a not found error", async () => {
-        jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(null);
+        jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(
+            null
+        );
 
         const promise = recommendationService.upvote(1);
 
-        expect(promise).rejects.toEqual({type: "not_found", message: ""})
-    })
+        expect(promise).rejects.toEqual({ type: "not_found", message: "" });
+        expect(recommendationRepository.updateScore).not.toHaveBeenCalled();
+    });
+});
+
+describe("Downvote recommendation tests", () => {
+    it("Should update score", async () => {
+        jest.spyOn(recommendationRepository, "find").mockResolvedValue({
+            id: 1,
+        });
+        jest.spyOn(
+            recommendationRepository,
+            "updateScore"
+        ).mockResolvedValueOnce({
+            score: 1,
+        });
+        jest.spyOn(recommendationRepository, "remove").mockImplementation(
+            () => {}
+        );
+        await recommendationService.downvote(1);
+
+        expect(recommendationRepository.updateScore).toHaveBeenCalledTimes(1);
+        expect(recommendationRepository.remove).not.toHaveBeenCalled();
+    });
+
+    it("Given a score that is less than -5, should remove the recommendation", async () => {
+        jest.spyOn(
+            recommendationRepository,
+            "updateScore"
+        ).mockResolvedValueOnce({
+            score: -6,
+        });
+
+        await recommendationService.downvote(1);
+
+        expect(recommendationRepository.updateScore).toHaveBeenCalledTimes(1);
+        expect(recommendationRepository.remove).toHaveBeenCalledTimes(1);
+    });
+
+    it("Given an id that doesn't exist, should throw a not found error", async () => {
+        jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(
+            null
+        );
+
+        const promise = recommendationService.downvote(1);
+
+        expect(promise).rejects.toEqual({ type: "not_found", message: "" });
+        expect(recommendationRepository.updateScore).not.toHaveBeenCalled();
+    });
 });
